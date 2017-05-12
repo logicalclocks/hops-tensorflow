4ddfc98ff99c08ce4c82d46ad364c17a9c1be80b
Tobias Johansson <tobias@johansson.xyz>
2017-05-12 16:42:02 +0200

    Bump version (#24)

--- yarntf/setup.py
+++ yarntf/setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
   name='yarntf',
-  version='0.0.3.dev3',
+  version='0.0.4.dev1-SNAPSHOT',
   description='Easy distributed TensorFlow on Hops Hadoop',
   long_description=long_description,
   url='https://github.com/hopshadoop/hops-tensorflow',
